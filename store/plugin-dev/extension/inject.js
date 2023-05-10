653049399b9b7442e1eb6d8b5754664a3e9ac9f0
ravikumar <ravikumar.ramanathan@valgenesis.com>
2023-05-10 20:38:18 +0530

    as

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -2,12 +2,10 @@
 	var URL_TO_PLUGIN = "https://github.com/ravikumar05031988/onlyoffice.github.io/tree/master/sdkjs-plugins/content/helloworld";
 	
 	var xhrObj = new XMLHttpRequest();
-	if ("withCredentials" in xhrObj) {
- 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", true);
-	//xhrObj.setRequestHeader('Access-Control-Allow-Origin', '*');
+	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", true);
+	xhrObj.setRequestHeader('Access-Control-Allow-Origin', '*');
 	xhrObj.send('');
-	}
-	var configObj = JSON.parse(xhrObj.responseText);
+   var configObj = JSON.parse(xhrObj.responseText);
 	configObj.baseUrl = URL_TO_PLUGIN;
 	window.Asc = window.Asc ? window.Asc : {};
 	window.Asc.extensionPlugins = window.Asc.extensionPlugins ? window.Asc.extensionPlugins : [];
